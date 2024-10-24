0cfff82a5df2d35cf7355d9e40efdb786df88c99
Fai Alateeq <fairouzalatteq@gmail.com>
2024-10-24 15:02:27 -0500

    UserInterface added

---
+++ src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,15 @@
+package com.ps;
+
+public class UserInterface {
+    private Dealership dealership = new Dealership();
+
+    public UserInterface(Dealership dealership) {
+        this.dealership = dealership;
+    }
+
+    public void displayScreen(){
+
+    }
+
+
+}
